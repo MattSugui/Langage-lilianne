e2e61e6e29613128abcb004dfa955e8d1d4a45bb
MattSugui <69552216+MattSugui@users.noreply.github.com>
2021-12-27 18:33:38 +0800

    rsdsgfgdfgdhghgh

--- lilylang/Interpreter.cs
+++ lilylang/Interpreter.cs
@@ -103,31 +103,34 @@ namespace fonder.Lilian.New
                 ProgressCharacter = '\u2590',
                 BackgroundCharacter = '\u2588',
                 CollapseWhenFinished = false,
-                DisplayTimeInRealTime = true,
+                //DisplayTimeInRealTime = true,
             };
 
             //watch.Start();
-            using (var pbm = new ProgressBar(CurrentFile.Count, "Scanning tokens", opt))
+            using (var pbm = new ProgressBar(4, "Interpretation process", opt))
             {
-                
-                for (int i = 1; i < CurrentFile.Count + 1; i++)
+                using (var pba = pbm.Spawn(CurrentFile.Count, "Scanning tokens", opt))
                 {
-                    ScanTokens(CurrentFile[i - 1]);
-                    /*
-                    timerem.SecondsRemaining = (watch.Elapsed.Seconds / i) * (i - CurrentFile.Count);
-                    timerem.PercentComplete = int.Parse($"{i / CurrentFile.Count:0}");
-                    timerem.StatusDescription = $"{i} lines scanned";
-                    timerem.CurrentOperation = "Scanning lines";
-                    */
-                    //ulong o = (ulong)((watch.Elapsed.Seconds / i) * (CurrentFile.Count - i));
-                    //var p = (double)i / CurrentFile.Count;
-                    //string stat = $"Scanning tokens. {o} seconds remaining ";
-                    //pb.Report(p, stat);
-                    /*WriteLine($"\n{o} seconds left*/
-                    //pbm.EstimatedDuration = TimeSpan.FromSeconds(o);
-                    pbm.Tick(TimeSpan.FromSeconds(((pbm.CurrentTick / 20) / i) * (CurrentFile.Count - i)), $"Scanning tokens");
-                    
+                    for (int i = 1; i < CurrentFile.Count + 1; i++)
+                    {
+                        ScanTokens(CurrentFile[i - 1]);
+                        /*
+                        timerem.SecondsRemaining = (watch.Elapsed.Seconds / i) * (i - CurrentFile.Count);
+                        timerem.PercentComplete = int.Parse($"{i / CurrentFile.Count:0}");
+                        timerem.StatusDescription = $"{i} lines scanned";
+                        timerem.CurrentOperation = "Scanning lines";
+                        */
+                        //ulong o = (ulong)((watch.Elapsed.Seconds / i) * (CurrentFile.Count - i));
+                        //var p = (double)i / CurrentFile.Count;
+                        //string stat = $"Scanning tokens. {o} seconds remaining ";
+                        //pb.Report(p, stat);
+                        /*WriteLine($"\n{o} seconds left*/
+                        //pbm.EstimatedDuration = TimeSpan.FromSeconds(o);
+                        pba.Tick();
+
+                    }
                 }
+                pbm.Tick();
             }
             //watch.Stop();
             WriteLine("complet");
