6569dd75b48c885c4d93b8703565de3bffc4bdbe
MattSugui <69552216+MattSugui@users.noreply.github.com>
2022-02-06 18:42:29 +0800

    Lilian: use FitnessGram pacer test copypasta as wrapping test

--- lilylang/Core.cs
+++ lilylang/Core.cs
@@ -230,7 +230,7 @@ public static class Programme
         ReadKey(true);
         LaunchUI();
         ApplicationTitle = "Fonder Lilian Language Environment";
-        PlainTextScreen("Interim Graphix Stage.");
+        PlainTextScreen("The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.");
         Environment.Exit(0);
 #else
         if (args.Length == 0) goto REPLLoop;
